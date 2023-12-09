dccaec2f241f33f9bdf59d2408c1023914c552dc
mazharul-15 <mazharulislamhstu15@gmail.com>
2023-12-09 21:53:05 +0600

    hello world print

---
+++ Third.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Third {
+    public static void main(String args[]) {
+        int a, b;
+
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a + b +"\n");
+    }
+}
