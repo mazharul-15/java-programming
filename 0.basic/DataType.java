4b43c6a142a00fbf38f78e9d74f3f30ade3884c9
mazharul-15 <mazharulislamhstu15@gmail.com>
2023-12-09 22:14:17 +0600

    java data type

---
+++ 0.basic/DataType.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class DataType {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        boolean bl = true; /// 1 bit
+
+        byte b = 4; /// 8 bit
+        char c = 'A'; /// 16 bit
+
+        short s = 34; /// 16 bit
+        int a = 343487; /// 32 bit
+        long ll = 34353252; /// 64 bit
+
+        float fl = 3435.55f;  /// 32 bit
+        double dl = 3434234.34322; // 64 bit
+        
+        System.out.println("boolean: "+ bl);
+        System.out.println("byte: " + b);
+        System.out.println("character: " + c);
+        System.out.println("short: " + s);
+        System.out.println("intger: " + a);
+        System.out.println("long: " + ll);
+        System.out.println("float: " + fl);
+        System.out.println("double: " + dl);
+
+
+    }
+}
---
+++ 0.basic/Input.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Input {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int num = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(num+num2);
+    }
+}
+
+
+
