630c566aaca4d21e49e7899d0c7100edd7a46c66
mazharul-15 <mazharulislamhstu15@gmail.com>
2023-12-10 20:53:48 +0600

    character input in java

---
+++ 0.basic/InputInJava.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class InputInJava {
+    
+    private char ch;
+    private double dl;
+    private byte bt;
+
+    /// user-defined methods
+    public void setValue() {
+        
+        Scanner scn = new Scanner(System.in);
+
+        this.ch = scn.next().charAt(0);
+        this.dl = scn.nextDouble();
+        this.bt = scn.nextByte();
+    }
+
+    public void getValue() {
+        
+        System.out.println("Character: " + ch);
+        System.out.println("Double: " + dl);
+        System.out.println("Byte: " + bt);
+        System.out.print(100);
+    }
+
+    /// main functin
+    public static void main(String[] args) {
+
+        InputInJava obj = new InputInJava();
+        obj.setValue();
+        obj.getValue();
+
+    }
+}
