ce6391643be775147d7362003d6eccc4757dcd13
mazharul-15 <mazharulislamhstu15@gmail.com>
2023-12-13 23:14:58 +0600

    string builder

---
+++ 3.strings/StringBuilders.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class StringBuilders {
+    
+    /// main function
+    public static void main(String args[]) {
+
+        Scanner scn = new Scanner(System.in);
+
+        StringBuilder str = new StringBuilder(scn.nextLine()); //= new StringBuilder();
+
+        /// str.append(scn.nexLine())
+        str.setCharAt(str.length()-1, 'A');
+
+        System.out.println(str);
+        
+
+    }
+}
