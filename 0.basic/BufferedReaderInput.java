b476c3746b873c1dd33e71a77a8f9edab3c6400b
mazharul-15 <mazharulislamhstu15@gmail.com>
2023-12-13 20:31:55 +0600

    bufferedReader input in java

---
+++ 0.basic/BufferedReaderInput.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+
+public class BufferedReaderInput {
+    
+
+    /// main function
+    public static void main(String args[])
+        throws IOException
+    {
+        // input via BufferedReader
+        BufferedReader bfn = new BufferedReader(new InputStreamReader(System.in));
+        
+        int val = Integer.parseInt(bfn.readLine());
+        String str = bfn.readLine();
+        //int val = Integer.parseInt(bfn.readLine());
+
+        System.out.println(str);
+        System.out.println(val);
+
+
+    }
+}
