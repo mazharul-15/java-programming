120ea9622fdcbf887c111ce71d6a62aabd3f57db
mazharul-15 <mazharulislamhstu15@gmail.com>
2023-12-19 22:41:03 +0600

    abstraction in java

---
+++ 4.oops/Practice1.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class PenAndCar {
+
+    public int id;
+    public String color;
+
+    /// methods
+    public void display() {
+
+    }
+}
+
+class Pen extends PenAndCar {
+    
+    public String nip;
+    
+}
+
+class Car extends PenAndCar {
+
+    public String breakType;
+}
+
+
+public class Practice1 {
+    
+    /// main function
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ 4.oops/PracticeAbstron.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+abstract class Shape {
+    public String color;
+    
+    abstract void draw();
+
+    /// constractor of abstract class
+    Shape()
+    {
+
+    }
+
+    Shape(String color)
+    {
+        this.color = color;
+    }
+}
+
+class Circle extends Shape {
+    public int id;
+
+    /// constructor of concrete class
+    Circle() 
+    {
+        /// calling "Abstract class constructor"
+        super();
+    }
+
+    Circle(String color, int id)
+    {
+        super(color);
+        this.id = id;
+    }
+
+    @Override void draw() {
+        System.out.println("Draw the circle!!");
+        System.out.println(this.color + " " + this.id);
+    }
+}
+
+
+public class PracticeAbstron {
+        
+    /// main()
+    public static void main(String[] args)
+    {
+        Circle obj = new Circle("Red", 123);
+        obj.draw();
+    }
+}
