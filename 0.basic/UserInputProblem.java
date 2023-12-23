044167d13899121ed63db069657d4866e630aeb5
mazharul-15 <mazharulislamhstu15@gmail.com>
2023-12-23 21:58:28 +0600

    user input problem with interger & strings input

---
+++ 0.basic/UserInputProblem.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class UserInputProblem {
+    
+    // main()
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+
+        String fName, lName;
+        int age;
+        double weight;
+        
+        System.out.println("Enter your first name: ");
+        fName = scn.nextLine();
+        
+        System.out.println("Enter your age: ");
+        age = Integer.parseInt(scn.nextLine());
+
+        System.out.println("Enter your weight: ");
+        weight = Double.parseDouble(scn.nextLine());
+
+        System.out.println("Enter your last name: ");
+        lName = scn.nextLine();
+
+        scn.close();
+    }
+}
