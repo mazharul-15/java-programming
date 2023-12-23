c372b1c7637e8f3cee2e6a013f98f171812a32c2
mazharul-15 <mazharulislamhstu15@gmail.com>
2023-12-23 22:29:31 +0600

    input problem solve

--- 0.basic/BasicPractice.java
+++ 0.basic/BasicPractice.java
@@ -3,26 +3,28 @@ import java.util.*;
 class StudentInfo {
 
     private int id;
-    //private String name;
-    //private float gpa;
+    private String name;
+    private float gpa;
 
-    public void setValue(int id) {
+    public void setValue(int id, String name, float gpa) 
+    {
         this.id = id;
-        ///this.name = name;
-        //this.gpa = gpa;
+        this.name = name;
+        this.gpa = gpa;
     }
 
-    public void getValue() {
-        //System.out.print(this.id +" "+ this.name +" "+ this.gpa +"\n");
-        System.out.println(this.id);
+    public void getValue() 
+    {
+        System.out.print(this.id +" "+ this.name +" "+ this.gpa +"\n");
+        //System.out.println(this.id +": "+ this.gpa);
     }
 }
 
 public class BasicPractice {
     
     // main function
-    public static void main(String[] args) {
-
+    public static void main(String[] args) 
+    {
         Scanner scn = new Scanner(System.in);
         int studentId;
         String studentName;
@@ -31,18 +33,20 @@ public class BasicPractice {
         StudentInfo[] obj = new StudentInfo[4];
 
         /// set value to the object
-        for(int i = 0; i < obj.length; i++) {
-            studentId = scn.nextInt();
-            //studentName = scn.nextLine();
-            //studentGPA = scn.nextFloat();
+        for(int i = 0; i < obj.length; i++)
+        {
+            studentId = Integer.parseInt(scn.nextLine());
+            studentName = scn.nextLine();
+            studentGPA = Float.parseFloat(scn.nextLine());
 
             obj[i] = new StudentInfo();
-            //obj[i].setValue(studentId, studentName, studentGPA);
-            obj[i].setValue(studentId);
+            obj[i].setValue(studentId, studentName, studentGPA);
+            //obj[i].setValue(studentId, studentGPA);
         }
 
         // get value for every object
-        for(int i = 0; i < obj.length; i++) {
+        for(int i = 0; i < obj.length; i++)
+        {
             obj[i].getValue();
         }
 
