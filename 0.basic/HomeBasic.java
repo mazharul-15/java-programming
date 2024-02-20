84ec0c46ebbb2927ec4a00b87d4fd9416acae71b
mazharul-15 <mazharulislamhstu15@gmail.com>
2024-02-20 16:32:24 +0600

    Input output

---
+++ 0.basic/HomeBasic.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class HomeBasic {
+    
+    /// main function
+    public static void  main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+
+        String abrarMahmud;
+        abrarMahmud = scn.nextLine();
+
+        System.out.println(abrarMahmud);
+    }
+}
